3a2bea2bd42f80bae9a084047648e25c96a01170
LForchini <leonardo.forchini@gmail.com>
2022-01-18 13:55:28 +0000

    Heroku

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
-import Menu from "./src/models/Menu.model";
-import MenuItem from "./src/models/MenuItem.model";
-import Restaurant from "./src/models/Restaurant.model";
-import { sequelize } from "./src/sequelize";
+import { loadSeed } from "./src/sequelize";
 import express from "express";
 
 import RestaurantRoute from "./src/routes/Restuaurant.route";
@@ -11,59 +8,12 @@ import MenuItemRoute from "./src/routes/MenuItem.route";
 const app = express();
 const PORT: string | number = process.env.PORT || 3000;
 
-// app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use("/restaurants", RestaurantRoute);
 app.use("/menus", MenuRoute);
 app.use("/items", MenuItemRoute);
-
-interface RestaurantObj {
-  name: string;
-  image: string;
-  menus: MenuObj[];
-}
-interface MenuObj {
-  title: string;
-  items: MenuItemObj[];
-}
-interface MenuItemObj {
-  name: string;
-  price: number;
-}
-
-async function loadSeed() {
-  await sequelize.sync({ force: true });
-  const restaurants: RestaurantObj[] = require("./seed.json");
-
-  restaurants.forEach(async (restaurant_obj: RestaurantObj) => {
-    const restaurant = new Restaurant({
-      name: restaurant_obj.name,
-      image: restaurant_obj.image,
-    });
-    await restaurant.save();
-
-    restaurant_obj.menus.forEach(async (menu_obj: MenuObj) => {
-      const menu = new Menu({
-        title: menu_obj.title,
-        restaurantId: restaurant.id,
-        restaurant: restaurant,
-      });
-      await menu.save();
-
-      menu_obj.items.forEach(async (menuItem_obj: MenuItemObj) => {
-        const menuItem = new MenuItem({
-          name: menuItem_obj.name,
-          price: menuItem_obj.price,
-          menuId: menu.id,
-          menu: menu,
-        });
-        await menuItem.save();
-      });
-    });
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server started listening on port ${PORT}`);
--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,4 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
+import Restaurant from "./models/Restaurant.model";
+import Menu from "./models/Menu.model";
+import MenuItem from "./models/MenuItem.model";
 
 const location =
   process.env.NODE_ENV === "test" ? ":memory:" : "./restaurants.sqlite";
@@ -15,4 +18,50 @@ const sequelize = new Sequelize("database", "username", "password", {
   },
 });
 
-export { sequelize };
+interface RestaurantObj {
+  name: string;
+  image: string;
+  menus: MenuObj[];
+}
+interface MenuObj {
+  title: string;
+  items: MenuItemObj[];
+}
+interface MenuItemObj {
+  name: string;
+  price: number;
+}
+
+async function loadSeed() {
+  await sequelize.sync({ force: true });
+  const restaurants: RestaurantObj[] = require("./seed.json");
+
+  restaurants.forEach(async (restaurant_obj: RestaurantObj) => {
+    const restaurant = new Restaurant({
+      name: restaurant_obj.name,
+      image: restaurant_obj.image,
+    });
+    await restaurant.save();
+
+    restaurant_obj.menus.forEach(async (menu_obj: MenuObj) => {
+      const menu = new Menu({
+        title: menu_obj.title,
+        restaurantId: restaurant.id,
+        restaurant: restaurant,
+      });
+      await menu.save();
+
+      menu_obj.items.forEach(async (menuItem_obj: MenuItemObj) => {
+        const menuItem = new MenuItem({
+          name: menuItem_obj.name,
+          price: menuItem_obj.price,
+          menuId: menu.id,
+          menu: menu,
+        });
+        await menuItem.save();
+      });
+    });
+  });
+}
+
+export { sequelize, loadSeed };
