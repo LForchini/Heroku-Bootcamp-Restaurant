5585f1aae71697554cdccb882d448ac96e580584
LForchini <leonardo.forchini@gmail.com>
2022-01-24 15:41:59 +0000

    Heroku

--- index.ts
+++ index.ts
@@ -11,10 +11,10 @@ const PORT: string | number = process.env.PORT || 3000;
 app.use("/", express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(`Recieved ${req.method} request for ${req.path} with body `, req.body);
-  next();
-})
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   console.log(`Recieved ${req.method} request for ${req.path} with body `, req.body);
+//   next();
+// })
 
 app.use("/restaurants", RestaurantRoute);
 app.use("/menus", MenuRoute);
